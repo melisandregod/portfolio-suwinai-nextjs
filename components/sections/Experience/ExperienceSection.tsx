72c2d74209929c1d3fca45e608335509adb1f92d
melisandregod <momomeez01@gmail.com>
2025-05-13 23:08:18 +0700

    style: change color in experience section light mode

--- components/sections/Experience/ExperienceSection.tsx
+++ components/sections/Experience/ExperienceSection.tsx
@@ -11,13 +11,16 @@ export default function Experience() {
   return (
     <div className="m-4 overflow-hidden" id="Experience">
       <div className="flex flex-col items-center gap-10 my-10">
-        <h2 className="text-3xl lg:text-5xl font-bold mt-2 xl:mt-20" data-aos="zoom-in">
+        <h2
+          className="text-3xl lg:text-5xl font-bold mt-2 xl:mt-20"
+          data-aos="zoom-in"
+        >
           {title("title")}
         </h2>
         <div className="xl:flex flex-col justify-center xl:px-40">
           <div className="w-full flex flex-col h-full my-15 xl:my-30 justify-center">
             <div
-              className="absolute bg-white h-[55vh] w-2.5 z-[-1] rounded-2xl ml-21 drop-shadow-md drop-shadow-white"
+              className="absolute bg-primary dark:bg-white h-[55vh] w-2.5 z-[-1] rounded-2xl ml-21 drop-shadow-md drop-shadow-primary dark:drop-shadow-white"
               data-aos="fade-up"
               data-aos-duration="1000"
               data-aos-easing="linear"
@@ -29,7 +32,7 @@ export default function Experience() {
                   alt="test"
                   width={600}
                   height={400}
-                  className="w-[100] my-10 ml-10 mr-5 rounded-full border-white border-4 drop-shadow-md drop-shadow-white"
+                  className="w-[100] my-10 ml-10 mr-5 rounded-full border-primary dark:border-white border-4 drop-shadow-md drop-shadow-primary dark:drop-shadow-white"
                   data-aos="zoom-in"
                   data-aos-delay="500"
                 />
@@ -38,7 +41,9 @@ export default function Experience() {
                     {t(`${key}.date`)} | {t(`${key}.role`)}
                   </p>
                   <p className="text-sm">{t(`${key}.company`)}</p>
-                  <p className="hidden 2xl:flex text-sm ">{t(`${key}.description`)}</p>
+                  <p className="hidden 2xl:flex text-sm ">
+                    {t(`${key}.description`)}
+                  </p>
                 </div>
               </div>
             ))}
